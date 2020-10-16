124159058c2af5187b8151c3af913c1e9ad4eeed
Lalitus <tallitosan@gmail.com>
2020-10-16 03:07:40 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config({path: './config/config.env'})
+const hbs = require('express-handlebars')
+const session = require('express-session')
+const flash = require('express-flash')
+
+const app = express()
+
+// handlebars
+app.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
+
+// session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}))
+
+// flash
+app.use(flash())
+
+// global vars for flash
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success')
+  res.locals.error = req.flash('error')
+  next()
+})
+
+// parse & encode
+app.use(express.urlencoded({extended: false}))
+
+// routes
+// @desc    welcome page
+// @access  public
+app.get('/', (req, res, next) => res.render('pages/welcome'))
+
+// @desc    message processing
+// @access  public
+app.post('/process', (req, res, next) => {
+  const {name, email, phone, message} = req.body
+  console.log(req.body)
+  req.flash('success', 'Su mensaje se ha enviado correctamente')
+  res.redirect('/')
+})
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(
+  process.env.PORT,
+  console.log(`Running on ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
+)
