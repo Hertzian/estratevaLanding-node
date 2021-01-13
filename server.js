7cacb16bf5f53c7cdec8d1e1ed2f7f35b3898bc4
Lalitus <tallitosan@gmail.com>
2021-01-12 20:13:00 -0600

    added validation to /process, dotenv for globar vars, minor tweaks wit alerts in welcome page

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const express = require('express')
 const path = require('path')
 const dotenv = require('dotenv')
-dotenv.config({path: './config/config.env'})
+dotenv.config({ path: './config/config.env' })
 const hbs = require('express-handlebars')
 const session = require('express-session')
 const flash = require('express-flash')
+const { Validator } = require('node-input-validator')
 
 const app = express()
 
 // handlebars
-app.engine('hbs', hbs({
-  defaultLayout: 'main',
-  extname: 'hbs'
-}))
+app.engine(
+  'hbs',
+  hbs({
+    defaultLayout: 'main',
+    extname: 'hbs',
+  })
+)
 app.set('view engine', 'hbs')
 
 // session
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+)
 
 // flash
 app.use(flash())
@@ -33,7 +39,7 @@ app.use((req, res, next) => {
 })
 
 // parse & encode
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 // routes
 // @desc    welcome page
@@ -42,9 +48,25 @@ app.get('/', (req, res, next) => res.render('pages/welcome'))
 
 // @desc    message processing
 // @access  public
-app.post('/process', (req, res, next) => {
-  const {name, email, phone, message} = req.body
-  console.log(req.body)
+app.post('/process', async (req, res, next) => {
+  const validator = new Validator(req.body, {
+    nombre: 'required',
+    email: 'required|email',
+    telefono: 'required|phoneNumber',
+    mensaje: 'required',
+  })
+
+  const matched = await validator.check()
+
+  if (!matched) {
+    for (const error in validator.errors) {
+      req.flash('error', ` Verifique el campo ${error}`)
+    }
+
+    res.redirect('/')
+    return
+  }
+
   req.flash('success', 'Su mensaje se ha enviado correctamente')
   res.redirect('/')
 })
@@ -54,5 +76,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(
   process.env.PORT,
-  console.log(`Running on ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
+  console.log(
+    `Running on ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  )
 )
